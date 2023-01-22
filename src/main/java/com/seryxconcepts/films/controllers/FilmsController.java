fff29820c903968e1fbe3523ad6c7bd12086b434
seryxme <coolbams24@yahoo.com>
2023-01-22 11:07:16 +0100

    Updated CORS. Updated port number for ngrok.

--- src/main/java/com/seryxconcepts/films/controllers/FilmsController.java
+++ src/main/java/com/seryxconcepts/films/controllers/FilmsController.java
@@ -3,19 +3,16 @@ package com.seryxconcepts.films.controllers;
 import com.seryxconcepts.films.data.models.Film;
 import com.seryxconcepts.films.services.FilmService;
 import lombok.AllArgsConstructor;
-import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/v1/films")
 @AllArgsConstructor
 public class FilmsController {
@@ -27,7 +24,6 @@ public class FilmsController {
     public ResponseEntity<List<Film>> allFilms() {
         return new ResponseEntity<>(filmService.getAllFilms(), HttpStatus.OK);
     }
-
     @GetMapping("/{imdbId}")
     public ResponseEntity<Optional<Film>> getFilmBy(@PathVariable String imdbId) {
         return new ResponseEntity<>(filmService.getFilmBy(imdbId), HttpStatus.OK);
